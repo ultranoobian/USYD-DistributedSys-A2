cec769ee2c1afdbc4cdc7868a52ad32b6e48f5d4
Brenden Cai <caibrenden@gmail.com>
2019-04-15 00:32:48 +1000

    Added threaded connection handler for incoming connections

--- src/BlockchainServerRunnable.java
+++ src/BlockchainServerRunnable.java
@@ -1,4 +1,7 @@
+import java.io.*;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.regex.Pattern;
 
 public class BlockchainServerRunnable implements Runnable {
 
@@ -6,12 +9,93 @@ public class BlockchainServerRunnable implements Runnable {
     private Blockchain blockchain;
 
     public BlockchainServerRunnable(Socket clientSocket, Blockchain blockchain) {
-        // implement your code here
+        this.clientSocket = clientSocket;
+        this.blockchain = blockchain;
+
     }
 
     public void run() {
-        // implement your code here
+        if (BlockchainServer.FLAG_DEBUG) {
+            System.out.println(String.format("Thread running ID: %d", Thread.currentThread().getId()));
+        }
+
+        try {
+            serverHandler(blockchain, clientSocket.getInputStream(), clientSocket.getOutputStream());
+        } catch (IOException e) {
+            if (BlockchainServer.FLAG_DEBUG) {
+                System.err.println(String.format("IO Exception occurred! Thread ID: %d.", Thread.currentThread().getId()));
+            }
+            e.printStackTrace();
+        }
+
+
+        if (BlockchainServer.FLAG_DEBUG) {
+            System.out.println(String.format("Thread released ID: %d", Thread.currentThread().getId()));
+        }
     }
 
     // implement any helper method here if you need any
+
+    public void serverHandler(Blockchain blockchain, InputStream clientInputStream, OutputStream clientOutputStream) throws IOException {
+
+        BufferedReader inputReader = new BufferedReader(
+                new InputStreamReader(clientInputStream));
+        PrintWriter outWriter = new PrintWriter(clientOutputStream, false);
+
+        try {
+            String line;
+            boolean exitSignalReceived = false;
+
+            while (!exitSignalReceived) {
+                if ((line = inputReader.readLine()) != null) {
+                    // Do stuff when there is data
+                    String[] lineComponents = line.split(Pattern.quote("|"));
+                    if (lineComponents.length >= 1) {
+                        switch (lineComponents[0]) {
+                            case "tx":
+                                // Add transaction (if valid).
+                                if (blockchain.addTransaction(line)) {
+                                    if (BlockchainServer.FLAG_DEBUG) System.out.println("Successful TX");
+                                    outWriter.print("Accepted\n\n");
+                                    outWriter.flush();
+
+                                } else {
+                                    if (BlockchainServer.FLAG_DEBUG) {
+                                        System.out.println(String.format("Unsuccessful TX: %s", line));
+                                    }
+                                    outWriter.print("Rejected\n\n");
+                                    outWriter.flush();
+                                }
+                                break;
+                            case "pb":
+                                // Print current blockchain.
+                                outWriter.print(blockchain.toString() + "\n");
+                                outWriter.flush();
+                                break;
+                            case "cc":
+                                // Close connection
+                                if (BlockchainServer.FLAG_DEBUG) System.out.println("Client closed connection");
+                                exitSignalReceived = true;
+                                break;
+                            default:
+                                if (BlockchainServer.FLAG_DEBUG)
+                                    System.out.println(String.format("Unknown Command %s", line));
+                                outWriter.print("Error\n\n");
+                                outWriter.flush();
+                                break;
+                        }
+                    }
+                } else {
+                    if (BlockchainServer.FLAG_DEBUG) System.out.println("Null received");
+                    break;
+                }
+            }
+        } catch (SocketException se) {
+            System.err.println("Socket Exception: Connection was reset.");
+        } finally {
+            inputReader.close();
+            outWriter.close();
+        }
+
+    }
 }
